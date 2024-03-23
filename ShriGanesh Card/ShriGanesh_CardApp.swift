0b5415ca04248ea471342255f2df06ef527fc00c
ShreeGaneshPurohit <89515358+ShreeGaneshPurohit@users.noreply.github.com>
2024-03-23 23:13:40 +0530

    Initial Commit

---
+++ ShriGanesh Card/ShriGanesh_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShriGanesh_CardApp.swift
+//  ShriGanesh Card
+//
+//  Created by SHRIGANESH PUROHIT on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ShriGanesh_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
