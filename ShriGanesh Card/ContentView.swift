068dc2dac3f93d5bcaf3ec0fb28f63830dabcfea
ShreeGaneshPurohit <89515358+ShreeGaneshPurohit@users.noreply.github.com>
2024-04-02 14:26:38 +0530

    Initial commit

--- ShriGanesh Card/ContentView.swift
+++ ShriGanesh Card/ContentView.swift
@@ -9,16 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            
+            Color(red: 0.09, green: 0.63, blue: 0.52)                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("SHRIGANESH")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit).frame(width: 150, height: 150).clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+
+                Text("ShriGanesh Purohit").font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                Text("Ios Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+
+                InfoView(text: "+91 9404457569", imageName: "phone.fill")
+                InfoView(text: "shriganeshpurohit@gmail.com", imageName: "envelope.fill")
+
+                
+            }
         }
-        .padding()
+        
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
